d2129cdf5a9f71f370967fcbb1233a7dfc043614
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-06 12:32:41 -0300

    Update PixTerm.c

--- PixTerm.c
+++ PixTerm.c
@@ -4,10 +4,11 @@
 #include "inc/ws2812b.h"
 #include "inc/ssd1306.h"
 #include <stdlib.h>
+#include "hardware/clocks.h"
 
 int main()
 {
     set_sys_clock_khz(128000, false);
     while(true);
-    return EXIT_SUCCESSFUL;
+    return EXIT_SUCCESS;
 }
