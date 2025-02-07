e02210d0e3b7b122e65898b81ec138ae55347b25
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-06 23:25:07 -0300

    Implementação uart_init_all

--- inc/uart.c
+++ inc/uart.c
@@ -1,6 +1,9 @@
 #include "uart.h"
+#include "pico/stdlib.h"
 
-inline void uart_init_all(uart_inst_t *uart, uint8_t rx, uint8_t tx, uint16_t baudrate)
+void uart_init_all(uart_inst_t *uart, uint8_t rx, uint8_t tx, uint16_t baudrate)
 {
-    return; //TO DO
+    uart_init(uart, baudrate);
+    gpio_set_function(rx, GPIO_FUNC_UART);
+    gpio_set_function(tx, GPIO_FUNC_UART);
 }
