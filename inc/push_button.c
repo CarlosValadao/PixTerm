ec6d7fd17f75ffd685f8ae988eec21c8e7f430f9
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-08 01:38:53 -0300

    Incremento delay de debounce

--- inc/push_button.c
+++ inc/push_button.c
@@ -1,6 +1,8 @@
 #include "push_button.h"
 #include "pico/stdlib.h"
 
+#define DEBOUNCE_DELAY_MS 300
+
 volatile bool FIRST_IRQ_USE = true;
 volatile gpio_irq_callback_t PB_IRQ_CALLBACK = NULL;
 volatile uint32_t PB_DEBOUNCE_LAST_TIME = 0;
@@ -24,7 +26,7 @@ bool pb_is_button_pressed(uint8_t button_pin) { return (!gpio_get(button_pin)); 
 bool pb_is_debounce_delay_over()
 {
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
-    bool debounce_time_elapsed = (current_time - PB_DEBOUNCE_LAST_TIME >= 200);
+    bool debounce_time_elapsed = (current_time - PB_DEBOUNCE_LAST_TIME >= DEBOUNCE_DELAY_MS);
     if(debounce_time_elapsed)
     {
         PB_DEBOUNCE_LAST_TIME = current_time;
