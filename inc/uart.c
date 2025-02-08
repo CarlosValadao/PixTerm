ceee6011426b5b82bbb97a54217276698abd5209
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-07 23:11:58 -0300

    Arquivo desnecessário

--- inc/uart.c
+++
@@ -1,9 +0,0 @@
-#include "uart.h"
-#include "pico/stdlib.h"
-
-void uart_init_all(uart_inst_t *uart, uint8_t rx, uint8_t tx, uint16_t baudrate)
-{
-    uart_init(uart, baudrate);
-    gpio_set_function(rx, GPIO_FUNC_UART);
-    gpio_set_function(tx, GPIO_FUNC_UART);
-}
