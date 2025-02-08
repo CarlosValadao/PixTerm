731687870c7d97a8172c4d30f24623d124ca526a
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-07 23:20:33 -0300

    Init commit

---
+++ inc/uart_usb.c
@@ -0,0 +1,11 @@
+#include "uart_usb.h"
+
+char uart_usb_blocking_getc()
+{
+    return '\0'; //TO DO
+}
+
+char *uart_usb_blocking_gets()
+{
+    return '\0'; //TO DO
+}
---
+++ inc/uart_usb.h
@@ -0,0 +1,8 @@
+#ifndef UART_USB_H
+#define UART_USB_H
+
+
+char uart_usb_blocking_getc();
+char *uart_usb_blocking_gets();
+
+#endif //UART_USB_H
