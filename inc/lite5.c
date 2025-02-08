9c51bcea5d5d186ac606e07e36e36e9a6e49a4bb
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-08 13:13:43 -0300

    Implementação parse_command

--- inc/lite5.c
+++ inc/lite5.c
@@ -1,9 +1,16 @@
 #include "lite5.h"
-
+#include <string.h>
+#include <stdlib.h>
 
 Command parse_command(const char *input)
 {
-    return; //TO DO
+    // invalid command
+    Command cmd = {.pattern = 0, .color = 0, .intensity = 0};
+    if(strlen(input) > 5) return cmd;
+    cmd.pattern = input[0];
+    cmd.color = input[1];
+    cmd.intensity = atoi(&input[2]);
+    return cmd;
 }
 
 void execute_command(const Command *cmd)
